f3d793b51c6262eb404e79dfbe2c10d89f9887d2
Slaton <slsp8752@colorado.edu>
2017-07-13 13:34:15 -0600

    Can now create new keyframes; keyframes have proper inputs for each field

--- interface/scripts/table.js
+++ interface/scripts/table.js
@@ -1,31 +1,60 @@
+function addFrame(table, color, transition, duration, position){
+	// Create Table
+	var frame = table.insertRow(position);
+	var colorCell = frame.insertCell(0);
+	var transitionCell = frame.insertCell(1);
+	var durationCell = frame.insertCell(2); 
+
+	// Create Color Picker
+	var colorPicker = document.createElement("button")
+	var picker = new jscolor(colorPicker);
+	picker.fromString(color);
+	colorPicker.style.width = "75px";
+	colorPicker.style.height = "20px";
+
+	// Create Transition Dropdown
+	var transitionDropdown = document.createElement("select");
+	var t1 = new Option();
+	var t2 = new Option();
+	t1.value = 1;
+	t2.value = 2;
+	t1.text = "None";
+	t2.text = "Fade";
+	transitionDropdown.options.add(t1);
+	transitionDropdown.options.add(t2);
+
+	// Create Duration Field
+	var durationInput = document.createElement("input");
+	durationInput.type = "number";
+	durationInput.min = 0.1;
+	durationInput.max = 10;
+	durationInput.step = 0.1;
+
+	// Append them to their respective cells
+	colorCell.appendChild(colorPicker);
+	transitionCell.appendChild(transitionDropdown);
+	durationCell.appendChild(durationInput);
+}
+
+function randomHexColor(){
+	return Math.floor(Math.random()*16777215).toString(16);
+}
+
 window.onload = function(){
+
 
 	var keyframeTable = document.getElementById("keyframes");
 
-	//IF no keyframe data can be pulled:
+	//If no keyframe data can be found, set up default table with one entry
+	addFrame(keyframeTable, "FF0000", "Fade", "10 Sec", keyframeTable.rows.length);
 
-	var defaultColor = 'FF0000';
-	var defaultTrans = 'Fade';
-	var defaultDurat = '10 Sec';
+	var newFrameRow = keyframeTable.insertRow(keyframeTable.rows.length);
+	var buttonCell = newFrameRow.insertCell(0);
+	var newFrameButton = document.createElement("button");
+	newFrameButton.innerHTML = "New Keyframe";
+	buttonCell.appendChild(newFrameButton);
 
-	var defaultFrame = keyframeTable.insertRow(1);
-	var defaultColorCell = defaultFrame.insertCell(0);
-	var defaultTransCell = defaultFrame.insertCell(1);
-	var defaultDuratCell = defaultFrame.insertCell(2);
+	newFrameButton.addEventListener("click", function(){ addFrame(keyframeTable, randomHexColor(), 'None', '1 Sec', keyframeTable.rows.length - 1); });
 
-
-	var defaultPicker = document.createElement('button')
-        var picker = new jscolor(defaultPicker);
-		picker.fromString(defaultColor);
-		defaultPicker.innerHTML = "";
-		defaultPicker.style.width = "50px";
-		defaultPicker.style.height = "20px";
-    
-
-	console.log(defaultPicker.innerHTML);
-
-	defaultColorCell.appendChild(defaultPicker);
-	defaultTransCell.innerHTML = defaultTrans;
-	defaultDuratCell.innerHTML = defaultDurat;
 
 }
